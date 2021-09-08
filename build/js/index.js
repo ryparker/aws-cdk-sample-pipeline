9015502a7cc114ad16d6bb91a47bff9cd88a7e2a
Ryan Parker <ryan.parker3@outlook.com>
2021-09-08 15:29:26 -0700

    fix(pipeline): added yarn build to synth commands

--- build/js/index.js
+++ build/js/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var aws_lambda_1 = require("@aws-cdk/aws-lambda");
 var core_1 = require("@aws-cdk/core");
 var pipelines_1 = require("@aws-cdk/pipelines");
+var aws_lambda_2 = require("@aws-cdk/aws-lambda");
 var app = new core_1.App();
 var stack = new core_1.Stack(app, "MyStack");
 var pipeline = new pipelines_1.CodePipeline(stack, "Pipeline", {
@@ -11,11 +13,12 @@ var pipeline = new pipelines_1.CodePipeline(stack, "Pipeline", {
         input: pipelines_1.CodePipelineSource.gitHub('ryparker/aws-cdk-sample-pipeline', 'main'),
         commands: [
             'yarn install',
+            'yarn build'
         ],
         primaryOutputDirectory: 'build/cloudformation',
     }),
 });
-pipeline.addWave('CustomWave', {
+var wave = pipeline.addWave('CustomWave', {
     pre: [new pipelines_1.ManualApprovalStep('ManualApproval')],
     post: [
         new pipelines_1.CodeBuildStep('CustomWaveAction', {
@@ -26,3 +29,11 @@ pipeline.addWave('CustomWave', {
         })
     ],
 });
+var lambdaStage = new core_1.Stage(stack, 'LambdaWaveStage');
+var lambdaStageStack = new core_1.Stack(lambdaStage, 'LambdaWaveStack');
+new aws_lambda_2.Function(lambdaStageStack, 'MyFunction', {
+    code: aws_lambda_2.Code.fromInline('console.log("hello world");'),
+    runtime: aws_lambda_1.Runtime.NODEJS_14_X,
+    handler: 'index.handler',
+});
+wave.addStage(lambdaStage);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { App, Stack } from "@aws-cdk/core";
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { App, Stack, Stage } from "@aws-cdk/core";
 import { CodePipeline, ShellStep, CodePipelineSource, ManualApprovalStep, CodeBuildStep } from "@aws-cdk/pipelines";
+import { Function, Code } from '@aws-cdk/aws-lambda';
 
 const app = new App();
 const stack = new Stack(app, "MyStack");
@@ -11,12 +13,13 @@ const pipeline = new CodePipeline(stack, "Pipeline", {
     input: CodePipelineSource.gitHub('ryparker/aws-cdk-sample-pipeline', 'main'),
     commands: [
       'yarn install',
+      'yarn build'
     ],
     primaryOutputDirectory: 'build/cloudformation',
   }),
 });
 
-pipeline.addWave('CustomWave', {
+const wave = pipeline.addWave('CustomWave', {
   pre: [new ManualApprovalStep('ManualApproval')],
   post: [
     new CodeBuildStep('CustomWaveAction', {
@@ -26,3 +29,12 @@ pipeline.addWave('CustomWave', {
       ],
     })],
 });
+
+const lambdaStage = new Stage(stack, 'LambdaWaveStage');
+const lambdaStageStack = new Stack(lambdaStage, 'LambdaWaveStack');
+new Function(lambdaStageStack, 'MyFunction', {
+  code: Code.fromInline('console.log("hello world");'),
+  runtime: Runtime.NODEJS_14_X,
+  handler: 'index.handler',
+});
+wave.addStage(lambdaStage);
