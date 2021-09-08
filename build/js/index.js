47de6eac5cf43d2cc9ff20256814e9963687b520
Ryan Parker <ryan.parker3@outlook.com>
2021-09-08 13:56:59 -0700

    initial commit

---
+++ build/js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@aws-cdk/core");
+var pipelines_1 = require("@aws-cdk/pipelines");
+var app = new core_1.App();
+var stack = new core_1.Stack(app, "MyStack");
+var pipeline = new pipelines_1.CodePipeline(stack, "Pipeline", {
+    pipelineName: "MyPipeline",
+    synth: new pipelines_1.ShellStep('Synth', {
+        input: pipelines_1.CodePipelineSource.gitHub('USACounts/infrastructure-cdk', 'main'),
+        commands: [
+            'yarn install',
+            'yarn build'
+        ],
+    }),
+});
+pipeline.addWave('CustomWave', {
+    pre: [new pipelines_1.ManualApprovalStep('ManualApproval')],
+    post: [
+        new pipelines_1.CodeBuildStep('CustomWaveAction', {
+            projectName: 'CustomWaveActionBuild',
+            commands: [
+                'echo Hello World'
+            ],
+        })
+    ],
+});
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { App, Stack } from "@aws-cdk/core";
+import { CodePipeline, ShellStep, CodePipelineSource, ManualApprovalStep, CodeBuildStep } from "@aws-cdk/pipelines";
+
+const app = new App();
+const stack = new Stack(app, "MyStack");
+
+const pipeline = new CodePipeline(stack, "Pipeline", {
+  pipelineName: "MyPipeline",
+  synth: new ShellStep('Synth', {
+    input: CodePipelineSource.gitHub('ryparker/aws-cdk-sample-pipeline', 'main'),
+    commands: [
+      'yarn install',
+    ],
+  }),
+});
+
+pipeline.addWave('CustomWave', {
+  pre: [new ManualApprovalStep('ManualApproval')],
+  post: [
+    new CodeBuildStep('CustomWaveAction', {
+      projectName: 'CustomWaveActionBuild',
+      commands: [
+        'echo Hello World'
+      ],
+    })],
+});
